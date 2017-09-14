d7fd964d0bfac53eae9adf0fee691fe7989d014f
阿瑜 <goish2468@gmail.com>
2017-09-15 02:32:18 +0800

    Code Creator _版三
    reference:https://fabhodev.wordpress.com/tag/uva-11483/
    版一版二 submmit到UVa Online judge 會有不能用gets的問題=>
    code.cpp: In function 'int main()':
    code.cpp:22:4: error: 'gets' was not declared in this scope
        gets(str);
        ^~~~
    code.cpp:22:4: note: suggested alternative: 'fgets'
        gets(str);
        ^~~~
        fgets
    版三:getline(cin,str)吃空白 屬於str流
    使用vector<string>存 傳給 printInput(string str)印出 printf("<input>\n");
    頭尾輸出造題目要求

--- 11483.cpp
+++ 11483.cpp
@@ -1,37 +1,74 @@
-#include<stdio.h>
-#include<string.h>
-
+#include <iostream>
+#include <cctype>
+#include <cstdio>
+#include <vector>
 using namespace std;
+int  caso = 1;
+void printHeader()
+{
+    printf("Case %d:\n", caso++);
+    printf("#include<string.h>\n");
+    printf("#include<stdio.h>\n");
+    printf("int main()\n");
+    printf("{\n");
+}
+
+void printInput(string str)
+{
+    printf("printf(\"");
+    for(int c=0; c<str.size(); c++)
+    {
+        if(isalnum(str[c]) || isspace(str[c]))
+          printf("%c",str[c]);
+        else if(str[c] == '\"') //
+        {
+        	//printf("hi");
+        	printf("\\");
+        	putchar('\"');
+        }
+        //else if(str[c] == '\'') 
+                //printf("\\\'");
+        else if(str[c] == '\\') //
+                printf("\\\\");
+        else  printf("%c",str[c]);       
+    }
+    printf("\\n\");\n");
+}
+
+void printFooter()
+{
+    printf("printf(\"\\n\");\n");
+    printf("return 0;\n");
+    printf("}\n");
+}
+
 int main()
 {
-	int n;		
-	char str[100];
-	int Case=0;
-	while(scanf("%d",&n)==1&&n!=0)
-	{
-		getchar();
-		printf("Case %d:\n", ++Case);
-		
-		printf("#include<string.h>\n");
-		printf("#include<stdio.h>\n");
-		printf("int main()\n");
-		printf("{\n");
-		
-		while(n--)
-		{
-			gets(str);
-			printf("printf(\"");
-			for(int i=0;str[i];i++)
-			{
-				if(str[i]=='\\'||str[i]=='\"')
-					printf("\\");
-				printf("%c",str[i]);
-			}
-			printf("\\n\");\n");
-		}
-		
-		printf("printf(\"\\n\");\n");
-		printf("return 0;\n}\n");
-	}
-	return 0;
+int lines;
+string str;
+
+while(scanf("%d", &lines))
+{
+    if(lines==0)
+       break;
+       
+    vector<string> vec;
+    //getline(cin,str);
+    getchar();
+    for(int a=0; a<lines; a++)
+    {
+        getline(cin, str);
+        //if(str=="\0") 
+        //{
+        	//a--;
+        	//continue;
+        //}
+        vec.push_back(str);
+    }
+    printHeader();    
+	for(int x=0; x<vec.size(); x++)
+        printInput(vec[x]);
+    printFooter();
+}
+return 0;
 }
