ca168b018d7ab25e4d79968e2b44d8d7064d687a
阿瑜 <goish2468@gmail.com>
2017-09-23 11:34:51 +0800

    Rotating Sentences
    長度不足:輸出空白
    AC
    ref:http://programming-study-notes.blogspot.tw/2014/02/uva-490-rotating-sentences.html?m=1

---
+++ 490版三_for_ACM.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <cstring>
+using namespace std;
+int main()
+{
+ //   freopen ("input.txt","rt",stdin);
+    char sentence[101][101];
+    int N=0,length[101];
+    int Max_length=0;
+    while (gets(sentence[N])) {
+        length[N] = strlen(sentence[N]);
+        if (length[N] > Max_length) Max_length = length[N];
+        N++;
+    }
+    for (int i=0; i<Max_length; i++){
+        for (int j=N-1; j>=0; j--){
+            if (i < length[j])
+                printf("%c",sentence[j][i]);
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
