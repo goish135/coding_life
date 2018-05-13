50dfbc4e68438a9f8fc9a7bb25720c13d7e39c77
阿瑜 <goish2468@gmail.com>
2018-05-13 16:57:40 +0800

    Update 13178.cpp

--- 13178.cpp
+++ 13178.cpp
@@ -1,33 +1,16 @@
-// Q13178
+// Q13178 
 #include<stdio.h>
-// #define long long  ll
-
 int main()
 {
-	//freopen("13178.in.txt","r",stdin);
-	//freopen("13178.out.txt","w",stdout);
 	int tc;
-	scanf("%d",&tc);
-	long long int n,N;
-	while(tc--) // + !=EOF
+	scanf("%d",&tc); 
+	while(tc--)
 	{
-		scanf("%lld",&n);
-		long long  sum=0,temp;
-		//printf("%lld\n",n);
-		for(long long int i=1;i<=n;i++)
-		{
-			temp = i;
-			sum+=temp%10;
-			while(1)
-			{
-				temp/=10;
-				sum+=temp%10;
-				if(temp==0) break;
-			}
-		}
-		N=sum;
-		if(N%3==0) printf("YES\n"); // + \n
-		else printf("NO\n"); // + \n
+		int n;
+		scanf("%d",&n);	
+		if(n%3==1) printf("NO\n");
+		else printf("YES\n");
 	}
-	return 0;	
-} 
+	return 0;
+}
+
