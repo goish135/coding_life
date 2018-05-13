c56696e11710decf1d1f951b8fb8e9de99365102
阿瑜 <goish2468@gmail.com>
2018-05-13 21:50:18 +0800

    題目名稱很長 描述很短
    輸入的部分 
     0 <=  n <= 101000 long long int 不夠存 採取 char 陣列 char num[1000+1]
    規律題 (除了 n=0 n=1)
    56,96,36,76,16 "五" 一循環

---
+++ uva13216.cpp
@@ -0,0 +1,73 @@
+// Q13216
+#include<stdio.h>
+#include<string.h>
+/*
+long long int ans(long long int num)
+{
+	long long int x = (66*66) % 100;
+	if(num==2)
+	{
+		return x;
+	}
+	else
+	{
+		for(long long int i=1;i<=(num-2);i++)
+		{
+			x = (x*66)%100;
+		}
+		return x;
+	}
+}
+*/
+/*
+n
+0:1
+1:66
+----
+2:56
+3:96
+4:36
+5:76
+6:16
+.
+.
+.
+*/
+int main()
+{
+	//freopen("13216.in.txt","r",stdin);
+	//freopen("13216.out.txt","w",stdout);
+	
+	int tc;
+	scanf("%d",&tc);
+	
+	while(tc--)
+	{
+		// long long int n; not enough to save
+		
+		// scanf("%lld",&n);
+		char num[1000+1];
+		scanf("%s",&num);
+		int len = strlen(num);
+		char r = num[len-1];
+		int rnum = r-'0';
+		
+		if(len==1&&rnum==0) printf("1\n");
+		else if(len==1&&rnum==1) printf("66\n");
+		else
+		{
+			//long long int as = ans(n);
+			//printf("%lld\n",as);
+			if(rnum==1) rnum = 11;
+			if(rnum==0) rnum = 10;
+			
+			int tp = (rnum-1)%5;
+			if(tp==1) printf("56\n");
+			else if(tp==2) printf("96\n");
+			else if(tp==3) printf("36\n");
+			else if(tp==4) printf("76\n");
+			else if(tp==0) printf("16\n");
+		}
+	}
+	return 0;
+}
